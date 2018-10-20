5f0db23ea368420632ed4c658b9cd33a5f470a05
okraskaj <jan.okraska@cern.ch>
2018-10-21 01:14:08 +0200

    working example of rgb blinking

--- raspberry_code/led-rgb.py
+++ raspberry_code/led-rgb.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-RED = 11
-GREEN = 13
-BLUE = 15
+RED = 23
+GREEN = 24
+BLUE = 25
 GPIO.setup(RED,GPIO.OUT)
 GPIO.output(RED,0)
 GPIO.setup(GREEN,GPIO.OUT)
@@ -13,6 +13,6 @@ GPIO.output(BLUE,0)
 
 while (True):
     for col in [RED,GREEN,BLUE]:
-        GPIO.output(col, 1)
+        GPIO.output(col, GPIO.HIGH)
         time.sleep(1)
-        GPIO.output(col, 0)
+        GPIO.output(col, GPIO.LOW)
