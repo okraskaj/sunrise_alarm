fd09e8ba82e704a686dc88814c6f4a400b7aefc1
Jan Okraska <jan.okraska@cern.ch>
2018-10-21 03:17:08 +0200

    final refactor and architecture for the future development

---
+++ alarm_controller/alarm.py
@@ -0,0 +1,11 @@
+from raspberry_code.transition_led_rgb import sunrise_sequence
+
+
+def play_sound():
+    pass
+    # TODO
+
+
+def run_alarm():
+    sunrise_sequence()
+    play_sound()
--- server/alarm_server.py
+++ server/alarm_server.py
@@ -10,6 +10,7 @@ ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
+# the part that connects with Alexa skill and takes data from the voice command
 @ask.intent('AddAlarm', mapping={'time': 'time'})
 def gpio_control(time):
     try:
@@ -18,8 +19,9 @@ def gpio_control(time):
         return statement('Could not translate the date.')
 
     print('alarm set to {}'.format(date_time))
-
+    # TODO: some kind of scheduling for the alarm should be here
     return statement('Setting alarm for {}'.format(time))
+
 
 if __name__ == '__main__':
         app.run(debug=True, host='0.0.0.0', port=5000)
